63569438b5956f4d36c339f07f6e2357e1e09628
seongmin36 <whtjdals3625@gmail.com>
2026-03-07 04:43:42 +0900

    feat: 타이밍 미들웨어 생성

---
+++ week01/src/middlewares/timer.middleware.ts
@@ -0,0 +1,17 @@
+// 요청 시간을 측정하여 로그에 출력하는 미들웨어
+export const timerMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const end = Date.now();
+    const duration = end - start;
+    console.log(
+      `[TIMER] ${req.originalUrl} - ${duration}ms - status: ${res.statusCode}`,
+    );
+  });
+  next();
+};
