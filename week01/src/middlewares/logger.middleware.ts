775d10c7866403729aebe14608c337e15c502218
seongmin36 <whtjdals3625@gmail.com>
2026-03-07 04:35:54 +0900

    docs: 로거 주석 설명 추가

--- week01/src/middlewares/logger.middleware.ts
+++ week01/src/middlewares/logger.middleware.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
+// 어떤 요청이 들어왔는지 출력
+// req -> logger -> 다음 미들웨어 흐름 확인용
 export const loggerMiddleware = (
   req: Request,
   _res: Response,
